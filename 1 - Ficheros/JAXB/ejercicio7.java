1163b2605b57a4bae158d482dd3bf2347f13cd46
unknown <alejandronavarro96@hotmail.com>
2020-10-21 21:07:24 +0200

    Añadido ejemplo JDBC, y ejemplos XML

---
+++ 1 - Ficheros/JAXB/ejercicio7.java
@@ -0,0 +1,54 @@
+package generated;
+
+import java.io.File;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+import generated.Libros.Libro;
+
+public class ejercicio7 {
+
+	public static void escribirMisLibros(File fichero) throws JAXBException {
+		
+		Libros misLibros = new Libros();
+		
+		Libro libroRubius = new Libro();
+		libroRubius.setAutor("El Rubius");
+		libroRubius.setEditorial("Nose");
+		libroRubius.setTitulo("El libro Troll del Rubius");
+		libroRubius.setPublicadoEn("2013");
+		
+		misLibros.getLibro().add(libroRubius);
+		
+		// Crear objeto clase JAXBContext
+		JAXBContext contexto;
+		// Le decimos al contexto que busce y enganche con objetos de la clase 'Libros'
+		contexto = JAXBContext.newInstance(Libros.class);
+		
+		// Serializar (o transformar) de Java a XML
+		Marshaller m = contexto.createMarshaller();
+		
+		// El método marshall pide el primer objeto que se quiere
+		// serializar, y el fichero donde se va a hacer
+		m.marshal(misLibros, fichero);
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		File fichero = new File("MisLibros.xml");
+		try {
+			escribirMisLibros(fichero);
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error al abrir el fichero con JAXB");
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ 2 - JDBC/EjemploBD.java
@@ -0,0 +1,74 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class EjemploBD {
+
+	
+	static Connection conexion = null;
+	static Statement stmt = null;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	      try {
+	    	//STEP 2: Register JDBC driver
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			// 3. Crear conexion
+			System.out.println("Intentamos conectar");
+			conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/Musicos?serverTimezone=UTC","root","123456");
+			System.out.println("Nos hemos conectado");
+			// 4 Crear Stametent
+			stmt = conexion.createStatement();
+			
+			/*
+			 * Crear base de datos, hay quye incluirla en la URL de conexion
+			String SQL = "CREATE DATABASE Musicos";
+			
+			stmt.executeUpdate(SQL);
+			*/
+			
+			/*
+			 * Crear tabla
+			 
+			String SQL = "CREATE TABLE Grupo (" + 
+					"Nombre VARCHAR(20) NOT NULL," + 
+					"Genero VARCHAR(20)," + 
+					"PRIMARY KEY (Nombre)" + 
+					");";
+			
+			stmt.executeUpdate(SQL);
+			*/
+			
+			/*
+			 * Añadir datos
+			String SQL = "INSERT INTO grupo(Nombre,Genero) VALUES(\"TWY\",\"Pop-punk\");";
+			
+			stmt.executeUpdate(SQL);
+			*/
+			
+			/*Obtener datos*/
+			String SQL = "Select * from grupo";
+			ResultSet resultado = stmt.executeQuery(SQL);
+			
+			while(resultado.next()) {
+				System.out.println(resultado.getString("Nombre"));
+				System.out.println(resultado.getString("Genero"));
+		
+			}
+			
+			
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
